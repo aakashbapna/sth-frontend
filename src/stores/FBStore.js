9efe9c684455c138814013681e63323108079f00
Boopathi Rajaa <me@boopathi.in>
2015-05-09 17:48:24 +0530

    dummy

--- src/stores/FBStore.js
+++ src/stores/FBStore.js
@@ -8,15 +8,11 @@ export default class FBStore extends Store {
 
 		this.register(fbActionIds.isLoggedIn, this.handleLogin);
 		this.register(fbActionIds.doLogin, this.handleLogin);
-		this.register(fbActionIds.delay, this.handleDelay);
 
 		this.state = {
 			login: false,
 			auth: null
 		};
-	}
-	handleDelay() {
-		console.log('delay Called');
 	}
 	handleLogin(resp) {
 		console.log(resp);
@@ -26,11 +22,5 @@ export default class FBStore extends Store {
 				auth: resp.authResponse
 			});
 		}
-	}
-	getLogin() {
-		return this.state.login;
-	}
-	getLoginCheck() {
-		return this.state.loginCheck;
 	}
 }
