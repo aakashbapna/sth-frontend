107c7604661673cef715660c041109a8e4295379
Vignesh Shanmugam <vignesh.shanmugam@flipkart.com>
2015-05-10 06:22:32 +0530

    Map Changes

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -5,22 +5,37 @@ let map;
 
 export default class extends React.Component {
 	state = {
-		marker: {
+		zoom: 16,
+		center: {
 			lat: this.props.location.lat,
 			lng: this.props.location.lng
 		}
 	}
+
+	_handle_marker_click () {
+	    this.setState({
+	      zoom: 8,
+	      center: this.refs.marker.getPosition(),
+	    });
+	}
+
 	render() {
+		const {props, state} = this,
+          {googleMapsApi, ...otherProps} = props;
+
 		return <GoogleMaps
 			ref='map'
 			containerProps={{
+				...otherProps,
 				style: {
 					height: '300px'
 				}
 			}}
 			googleMapsApi={"undefined" !== typeof google ? google.maps : null}
 			zoom={16}
-			center={this.props.location}>
+			center={state.center}>
+			<Marker ref="marker" position={state.center} title="Click to zoom"
+          		onClick={this._handle_marker_click.bind(this)} />
 		</GoogleMaps>
 	}
 }
